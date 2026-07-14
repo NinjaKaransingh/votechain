0de465cac4e22f64219984c45e4612a0f9bab05d
NinjaKaransingh <bkaransinghb@gmail.com>
2026-07-14 20:12:49 +0530

    resolved some issue

--- Backend/utils/createUserAccount.js
+++ Backend/utils/createUserAccount.js
@@ -37,7 +37,7 @@ const createUserAccount = async ({ name, email, password, role, state }) => {
       role: user.role,
     },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.expiresIn || "7d" },
+    { expiresIn: process.env.JWT_EXPIRES_IN || "7d" },
   );
 
   return { user, token };
